dff4f4f7fa2fb97c604fb6c37b1b1a12658816de
李浩轩 <15184917+lihaoxuan2943199381@user.noreply.gitee.com>
2025-12-09 15:17:38 +0800

    1

--- Driver_Layer/Src/locator_driver.c
+++
@@ -1,6 +0,0 @@
-//
-// Created by 马皓然 on 2025/11/5.
-//
-#include "locator_driver.h"
-
-Locator_Result_t lcResult;
--- Task/Inc/Task_cha_control.h
+++
@@ -1,7 +0,0 @@
-// Task_default.h
-#ifndef TASK_CHA_CONTROL_H_
-#define TASK_CHA_CONTROL_H_
-#include "cmsis_os.h"
-
-extern osMessageQueueId_t cha_speedqueueHandle;
-#endif /* TASK_CHA_CONTROL_H_ */
